5f74fd281b4dc751d1302f2d63cbe78f976502d5
Kristen <harunakristen@gmail.com>
2025-05-01 00:37:14 +0000

    validation

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,12 @@ let form = document.querySelector("#form");
 btn.addEventListener("click", function (event) {
   event.preventDefault();
 
+  // Clear previous error messages
+  document.querySelector("#empty").textContent = "";
+  document.querySelector("#pdob").textContent = "";
+  document.querySelector("#ppass").textContent = "";
+  document.querySelector("#e").textContent = "";
+
   let fname = document.querySelector("#fullname").value;
   let pWord = document.querySelector("#password").value;
   let dob = document.querySelector("#dob").value;
@@ -11,10 +17,19 @@ btn.addEventListener("click", function (event) {
 
   // 1. Check if any field is empty
   if (fname === "" || pWord === "" || dob === "" || email === "") {
-    alert("All fields must be filled");
+    let empty = document.querySelector("#empty");
+    empty.style.color = "red";
+    empty.textContent = "All fields must be filled!";
     return;
   }
 
+  // email includes@
+  if (!email.includes("@")) {
+    let e = document.querySelector("#e");
+    e.textContent = "must include '@'";
+    e.style.color = "red";
+    return;
+  }
   // 2. Check age
   let birthDate = new Date(dob);
   let today = new Date();
@@ -30,8 +45,8 @@ btn.addEventListener("click", function (event) {
 
   if (age < 18) {
     let pdob = document.querySelector("#pdob");
-    pdob.style.color = "green";
-    pdob.textContent = "must be above 18";
+    pdob.style.color = "red";
+    pdob.textContent = "Must be 18 or older!";
     return;
   }
 
@@ -39,7 +54,17 @@ btn.addEventListener("click", function (event) {
   let passwordCheck = /[!@#$%^&]/;
 
   if (!passwordCheck.test(pWord)) {
-    alert("Password must contain at least one special character: !@#$%^&");
+    let ppass = document.querySelector("#ppass");
+    ppass.style.color = "red";
+    ppass.textContent =
+      "Password must contain at least one special character (!@#$%^&)";
+    return;
+  }
+
+  if (pWord.length <= 7) {
+    let ppass = document.querySelector("#ppass");
+    ppass.style.color = "red";
+    ppass.textContent = "Password must be more than 7 characters";
     return;
   }
 
